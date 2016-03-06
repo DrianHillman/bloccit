1682e2ad1edc724102cc7bc4502a549bfa1c2c3c
Drian Hillman <dhillman24@gmail.com>
2016-03-06 18:49:25 -0500

    Assignment 30 Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
 require 'random_data'
 
+Post.find_or_create_by(title: "Drian's Title", body: "Body of the post.")
+# Used the following in the Rails Console to test uniqueness after multiple seeds:
+# Post.find_by(title: "Drian's Title", body: "Body of the post.")
+Comment.find_or_create_by(body: "Drian's Unique Comment")
+
+
 # Create Posts
 50.times do
   Post.create!(
