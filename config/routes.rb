0661f0b35451eba79d7fb6ecd7ca877a24d71bab
drianhillman <dhillman24@gmail.com>
2016-03-27 23:06:24 +0000

    Checkpoint 48 complete | Created app API

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,13 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
+  
   root 'welcome#index'
+  
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314192518) do
+ActiveRecord::Schema.define(version: 20160327215903) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20160314192518) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
