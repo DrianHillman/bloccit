1be33ec36cb1e65013f4f825331809596cde8320
drianhillman <dhillman24@gmail.com>
2017-07-23 19:25:21 +0000

    :seedling: update to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,13 @@ require 'random_data'
 
 # Create Users
 5.times do
-   User.create!(
-   name:     RandomData.random_name,
-   email:    RandomData.random_email,
-   password: RandomData.random_sentence
-   )
- end
- users = User.all
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
 
 # Create Topics
 15.times do
@@ -35,26 +35,27 @@ posts = Post.all
 # Create Comments
 100.times do
   Comment.create!(
-  user: users.sample,
-  post: posts.sample,
-  body: RandomData.random_paragraph
+    user: users.sample,
+    post: posts.sample,
+    body: RandomData.random_paragraph
   )
 end
 
- # Create an admin user
- admin = User.create!(
-   name:     'Admin User',
-   email:    'admin@example.com',
-   password: 'helloworld',
-   role:     'admin'
- )
+# Create an admin user
+demo_admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
  
- # Create a member
- member = User.create!(
-   name:     'Member User',
-   email:    'member@example.com',
-   password: 'helloworld'
- )
+# Create a member
+demo_member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld',
+  role: 'member'
+)
 
 
 puts "Seed finished"
@@ -62,4 +63,5 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
-puts "#{Vote.count} votes created"
+puts "1 demo admin created"
+puts "1 member created"
